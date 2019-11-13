04fb60026f8671b5a9d1c0685fb4e4fe12e6e559
Froiland Go <Froiland.Go@learning-health.co.uk>
2019-11-13 13:01:48 +0000

    Updated project_schedule definition

--- Database/Patches/DSMDB_Patch3.sql
+++ Database/Patches/DSMDB_Patch3.sql
@@ -5,13 +5,13 @@ create table `project_schedule` (
 	ends date COMMENT 'Ending date when reports will no longer be sent',
 	frequency smallint COMMENT 'Frequency of report sending. 0-Daily, 1-Weekly, 2-Monthly, 3-Yearly',
 	weeks char(7) COMMENT 'Comma separated week setting. i.e. 1,2,3,4',
-	is_monday boolean COMMENT 'True if report is to be sent on Mondays',
-	is_tuesday boolean COMMENT 'True if report is to be sent on Tuesdays',
-	is_wednesday boolean COMMENT 'True if report is to be sent on Wednesdays',
-	is_thursday boolean COMMENT 'True if report is to be sent on Thursdays',
-	is_friday boolean COMMENT 'True if report is to be sent on Fridays',
-	is_saturday boolean COMMENT 'True if report is to be sent on Saturdays',
-	is_sunday boolean COMMENT 'True if report is to be sent on Sundays',
+	is_monday tinyint(1) COMMENT '1 if report is to be sent on Mondays',
+	is_tuesday tinyint(1) COMMENT '1 if report is to be sent on Tuesdays',
+	is_wednesday tinyint(1) COMMENT '1 if report is to be sent on Wednesdays',
+	is_thursday tinyint(1) COMMENT '1 if report is to be sent on Thursdays',
+	is_friday tinyint(1) COMMENT '1 if report is to be sent on Fridays',
+	is_saturday tinyint(1) COMMENT '1 if report is to be sent on Saturdays',
+	is_sunday tinyint(1) COMMENT '1 if report is to be sent on Sundays',
     primary key data_sharing_manager_schedule_uuid (uuid)
 ) comment 'Information regarding project schedule';
 
