5d82e8819c85da5ad5b80b6aab4b8e4b4c6355e8
Froiland Go <Froiland.Go@learning-health.co.uk>
2019-12-10 21:36:11 +0000

    Adding Scheduler component

--- FrontEndv8/src/app/app-menu.service.ts
+++ FrontEndv8/src/app/app-menu.service.ts
@@ -2,13 +2,15 @@ import {Injectable} from '@angular/core';
 import {Routes} from '@angular/router';
 import {AbstractMenuProvider, MenuOption} from 'dds-angular8';
 import {OrganisationComponent} from "./organisation/organisation/organisation.component";
+import {SchedulerComponent} from "./scheduler/scheduler/scheduler.component";
 
 @Injectable()
 export class AppMenuService implements  AbstractMenuProvider {
   static getRoutes(): Routes {
     return [
       { path: '', redirectTo : '/organisations/organisations', pathMatch: 'full' }, // Default route
-      { path: 'organisations/:mode', component: OrganisationComponent, data: {role: 'Viewer'}}
+      { path: 'organisations/:mode', component: OrganisationComponent, data: {role: 'Viewer'}},
+      { path: 'scheduler', component: SchedulerComponent, data: {role: 'Viewer'}}
     ];
   }
 
@@ -23,6 +25,7 @@ export class AppMenuService implements  AbstractMenuProvider {
   getMenuOptions(): MenuOption[] {
     return [
       {caption: 'Organisations', state: 'organisations/organisations', icon: 'library_books'},
+      {caption: 'Scheduler', state: 'scheduler', icon: 'library_books'},
     ];
   }
 }
