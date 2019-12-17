endeavourhealth-discovery/DataSharingManager

commit 9a54a64bb65357e4239e88fdf138508be81eaaae

Use new Audit logic to get differences in linked items

API/src/main/java/org/endeavourhealth/datasharingmanager/api/DAL/MasterMappingDAL.java
@@ -1,9 +1,13 @@
 package org.endeavourhealth.datasharingmanager.api.DAL;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import org.endeavourhealth.common.security.datasharingmanagermodel.models.database.CohortEntity;
+import org.endeavourhealth.common.security.datasharingmanagermodel.models.database.DatasetEntity;
 import org.endeavourhealth.common.security.datasharingmanagermodel.models.database.MasterMappingEntity;
 import org.endeavourhealth.common.security.datasharingmanagermodel.models.enums.MapType;
 import org.endeavourhealth.common.security.datasharingmanagermodel.models.json.*;
 import org.endeavourhealth.common.security.usermanagermodel.models.ConnectionManager;
+import org.endeavourhealth.uiaudit.logic.AuditCompareLogic;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
@@ -56,31 +60,52 @@ public class MasterMappingDAL {
         }
     }
 
-    public void updateDataSetMappings(JsonDataSet updatedDataSet, List<String> oldDataSetDpas) throws Exception {
+    public void updateDataSetMappings(JsonDataSet updatedDataSet, DatasetEntity oldDataset) throws Exception {
         // Convert Map<UUID, String> to List<String>
-        List<String> updatedCohortDpas = new ArrayList<String>();
-        updatedDataSet.getDpas().forEach((k, v) -> updatedCohortDpas.add(k.toString()));
+        List<String> updatedDatasetDPAs = new ArrayList<String>();
+        updatedDataSet.getDpas().forEach((k, v) -> updatedDatasetDPAs.add(k.toString()));
 
-        updateMappings(true, updatedDataSet.getUuid(), oldDataSetDpas, updatedCohortDpas, MapType.COHORT.getMapType(), MapType.DATAPROCESSINGAGREEMENT.getMapType());
+        List<String> removedMappings = new ArrayList<>();
+        List<String> addedMappings = new ArrayList<>();
+
+        updateMappings(true, updatedDataSet.getUuid(), oldDataset.getDpas(), updatedDatasetDPAs, MapType.DATASET.getMapType(), MapType.DATAPROCESSINGAGREEMENT.getMapType(), removedMappings, addedMappings);
     }
 
-    public void updateCohortMappings(JsonCohort updatedCohort, List<String> oldCohortDpas) throws Exception {
+    public JsonNode updateCohortMappings(JsonCohort updatedCohort, CohortEntity oldCohort, JsonNode auditJson) throws Exception {
         // Convert Map<UUID, String> to List<String>
         List<String> updatedCohortDpas = new ArrayList<String>();
         updatedCohort.getDpas().forEach((k, v) -> updatedCohortDpas.add(k.toString()));
 
-        updateMappings(true, updatedCohort.getUuid(), oldCohortDpas, updatedCohortDpas, MapType.COHORT.getMapType(), MapType.DATAPROCESSINGAGREEMENT.getMapType());
+        List<String> removedMappings = new ArrayList<>();
+        List<String> addedMappings = new ArrayList<>();
+
+        updateMappings(true, updatedCohort.getUuid(), oldCohort.getDpas(), updatedCohortDpas, MapType.COHORT.getMapType(), MapType.DATAPROCESSINGAGREEMENT.getMapType(), removedMappings, addedMappings);
+
+        auditJson = appendToJson("RemovedDPAs", removedMappings, auditJson);
+        auditJson = appendToJson("AddedDPAs", addedMappings, auditJson);
+
+        return auditJson;
+
     }
 
-    private void updateMappings(boolean thisItemIsChild, String thisItem, List<String> oldMappings, List<String> updatedMappings, Short thisMapTypeId, Short otherMapTypeId) throws  Exception {
-        ArrayList<String> removedMappings = new ArrayList<String>();
+    private JsonNode appendToJson(String fieldName, List<String> mappings, JsonNode auditJson) throws Exception {
+        if (!mappings.isEmpty()) {
+            return new AuditCompareLogic().generateListDifferenceAuditJson(auditJson, fieldName, mappings, "dpa");
+        }
+
+        return auditJson;
+    }
+
+    private void updateMappings(boolean thisItemIsChild, String thisItem, List<String> oldMappings,
+                                List<String> updatedMappings, Short thisMapTypeId, Short otherMapTypeId,
+                                List<String> removedMappings, List<String> addedMappings) throws  Exception {
+
         for (String oldMapping : oldMappings) {
             if (!updatedMappings.contains(oldMapping)) {
                 removedMappings.add(oldMapping);
             }
         }
 
-        ArrayList<String> addedMappings = new ArrayList<String>();
         for (String updatedMapping : updatedMappings) {
             if (!oldMappings.contains(updatedMapping)) {
                 addedMappings.add(updatedMapping);

API/src/main/java/org/endeavourhealth/datasharingmanager/api/DAL/CohortDAL.java
@@ -1,5 +1,6 @@
 package org.endeavourhealth.datasharingmanager.api.DAL;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import org.endeavourhealth.common.security.datasharingmanagermodel.models.DAL.SecurityMasterMappingDAL;
 import org.endeavourhealth.common.security.datasharingmanagermodel.models.database.CohortEntity;
 import org.endeavourhealth.common.security.datasharingmanagermodel.models.enums.MapType;
@@ -58,7 +59,7 @@ public class CohortDAL {
         CohortEntity oldCohortEntity = entityManager.find(CohortEntity.class, cohort.getUuid());
         oldCohortEntity.setDpas(new SecurityMasterMappingDAL().getParentMappings(cohort.getUuid(), MapType.COHORT.getMapType(), MapType.DATAPROCESSINGAGREEMENT.getMapType()));
         CohortEntity newCohort = new CohortEntity(cohort);
-        String auditJson = new AuditCompareLogic().getAuditJson("Cohort edited", oldCohortEntity, newCohort);
+        JsonNode auditJson = new AuditCompareLogic().getAuditJsonNode("Cohort edited", oldCohortEntity, newCohort);
 
         try {
             entityManager.getTransaction().begin();
@@ -67,7 +68,7 @@ public class CohortDAL {
             oldCohortEntity.setDescription(cohort.getDescription());
             oldCohortEntity.setTechnicalDefinition(cohort.getTechnicalDefinition());
 
-            new MasterMappingDAL().updateCohortMappings(cohort, oldCohortEntity.getDpas());
+            auditJson = new MasterMappingDAL().updateCohortMappings(cohort, oldCohortEntity, auditJson);
 
             new UIAuditJDBCDAL().addToAuditTrail(userProjectId,
                     AuditAction.EDIT, ItemType.COHORT, null, null, auditJson);
@@ -93,9 +94,10 @@ public class CohortDAL {
             cohortEntity.setTechnicalDefinition(cohort.getTechnicalDefinition());
             cohortEntity.setUuid(cohort.getUuid());
 
-            new MasterMappingDAL().updateCohortMappings(cohort, null);
+            JsonNode auditJson = new AuditCompareLogic().getAuditJsonNode("Cohort Created", null, cohortEntity);
 
-            String auditJson = new AuditCompareLogic().getAuditJson("Cohort Created", null, cohortEntity);
+            new MasterMappingDAL().updateCohortMappings(cohort, null, auditJson);
+
             new UIAuditJDBCDAL().addToAuditTrail(userProjectId,
                     AuditAction.ADD, ItemType.COHORT, null, null, auditJson);
 
@@ -114,7 +116,7 @@ public class CohortDAL {
 
         try {
             CohortEntity oldCohortEntity = entityManager.find(CohortEntity.class, uuid);
-            String auditJson = new AuditCompareLogic().getAuditJson("Cohort deleted", oldCohortEntity, null);
+            JsonNode auditJson = new AuditCompareLogic().getAuditJsonNode("Cohort deleted", oldCohortEntity, null);
 
             CohortEntity cohortEntity = entityManager.find(CohortEntity.class, uuid);
             entityManager.getTransaction().begin();

API/src/main/java/org/endeavourhealth/datasharingmanager/api/DAL/DatasetDAL.java
@@ -1,5 +1,6 @@
 package org.endeavourhealth.datasharingmanager.api.DAL;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import org.endeavourhealth.common.security.datasharingmanagermodel.models.DAL.SecurityMasterMappingDAL;
 import org.endeavourhealth.common.security.datasharingmanagermodel.models.database.DatasetEntity;
 import org.endeavourhealth.common.security.datasharingmanagermodel.models.enums.MapType;
@@ -58,7 +59,8 @@ public class DatasetDAL {
         DatasetEntity oldDataSetEntity = entityManager.find(DatasetEntity.class, dataset.getUuid());
         oldDataSetEntity.setDpas(new SecurityMasterMappingDAL().getParentMappings(dataset.getUuid(), MapType.DATASET.getMapType(), MapType.DATAPROCESSINGAGREEMENT.getMapType()));
         DatasetEntity newDataSet = new DatasetEntity(dataset);
-        String auditJson = new AuditCompareLogic().getAuditJson("Data set edited", oldDataSetEntity, newDataSet);
+
+        JsonNode auditJson = new AuditCompareLogic().getAuditJsonNode("Data set edited", oldDataSetEntity, newDataSet);
 
         try {
             entityManager.getTransaction().begin();
@@ -66,7 +68,7 @@ public class DatasetDAL {
             oldDataSetEntity.setDescription(dataset.getDescription());
             oldDataSetEntity.setTechnicalDefinition(dataset.getTechnicalDefinition());
 
-            new MasterMappingDAL().updateDataSetMappings(dataset, oldDataSetEntity.getDpas());
+            new MasterMappingDAL().updateDataSetMappings(dataset, oldDataSetEntity);
 
             new UIAuditJDBCDAL().addToAuditTrail(userProjectId,
                     AuditAction.EDIT, ItemType.DATASET, null, null, auditJson);
@@ -94,7 +96,7 @@ public class DatasetDAL {
 
             new MasterMappingDAL().updateDataSetMappings(dataset, null);
 
-            String auditJson = new AuditCompareLogic().getAuditJson("Data set created", null, dataSetEntity);
+            JsonNode auditJson = new AuditCompareLogic().getAuditJsonNode("Data set created", null, dataSetEntity);
             new UIAuditJDBCDAL().addToAuditTrail(userProjectId,
                     AuditAction.ADD, ItemType.DATASET, null, null, auditJson);
 
@@ -115,7 +117,7 @@ public class DatasetDAL {
 
         try {
             DatasetEntity oldDataSetEntity = entityManager.find(DatasetEntity.class, uuid);
-            String auditJson = new AuditCompareLogic().getAuditJson("Data set deleted", oldDataSetEntity, null);
+            JsonNode auditJson = new AuditCompareLogic().getAuditJsonNode("Data set deleted", oldDataSetEntity, null);
             entityManager.getTransaction().begin();
             entityManager.remove(oldDataSetEntity);
 

API/src/main/webapp/WEB-INF/web.xml
@@ -104,7 +104,7 @@
             <url-pattern>/api/*</url-pattern>
         </web-resource-collection>
         <auth-constraint>
-            <role-name>dsa-manager:api</role-name>
+            <role-name>eds_user</role-name>
         </auth-constraint>
     </security-constraint>
 

commit c4fed74b89a132cb7b30f6d2aba7b9414917bfba

Fix for bug introduced in 9a54a64
NullPointerException when adding cohort.

API/src/main/java/org/endeavourhealth/datasharingmanager/api/DAL/MasterMappingDAL.java
@@ -75,7 +75,7 @@ public class MasterMappingDAL {
     public JsonNode updateCohortMappings(JsonCohort updatedCohort, CohortEntity oldCohort, JsonNode auditJson, EntityManager entityManager) throws Exception {
 
         // DPAs
-        auditJson = updateMappingsAndGetAudit(true, updatedCohort.getUuid(), oldCohort.getDpas(),
+        auditJson = updateMappingsAndGetAudit(true, updatedCohort.getUuid(), (oldCohort == null ? null: oldCohort.getDpas()),
                 updatedCohort.getDpas(), MapType.COHORT.getMapType(), MapType.DATAPROCESSINGAGREEMENT.getMapType(), auditJson, entityManager);
 
         return auditJson;
@@ -94,15 +94,24 @@ public class MasterMappingDAL {
                                 List<String> updatedMappings, Short thisMapTypeId, Short otherMapTypeId,
                                 List<String> removedMappings, List<String> addedMappings, EntityManager entityManager) throws Exception {
 
-        for (String oldMapping : oldMappings) {
-            if (!updatedMappings.contains(oldMapping)) {
-                removedMappings.add(oldMapping);
+        if (oldMappings == null || oldMappings.isEmpty()) {
+            // No old mappings; any that exist are new
+            addedMappings = updatedMappings;
+        } else if (updatedMappings == null || updatedMappings.isEmpty()) {
+            // No mappings in updated; any in old have been removed
+            removedMappings = oldMappings;
+        } else {
+            // old and updated mappings exist; check what has changed
+            for (String oldMapping : oldMappings) {
+                if (!updatedMappings.contains(oldMapping)) {
+                    removedMappings.add(oldMapping);
+                }
             }
-        }
 
-        for (String updatedMapping : updatedMappings) {
-            if (!oldMappings.contains(updatedMapping)) {
-                addedMappings.add(updatedMapping);
+            for (String updatedMapping : updatedMappings) {
+                if (!oldMappings.contains(updatedMapping)) {
+                    addedMappings.add(updatedMapping);
+                }
             }
         }
 